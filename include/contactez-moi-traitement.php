82e95c874814e5bd14b1ee50a2eb33a901045da4
arcausin <82374375+arcausin@users.noreply.github.com>
2021-06-09 09:57:11 +0200

    le con

--- include/contactez-moi-traitement.php
+++ include/contactez-moi-traitement.php
@@ -67,7 +67,7 @@ if (isset($_POST['submit'])) { // si le bouton Envoyer est appuyé
 				</html>
 				";
 				// envoie du mail
-				$email2 = "arcausin@gmail.com"
+				$email2 = "arcausin@gmail.com";
 				if (mail($email, $subjectTo, $corpsEmailTo, $headers)) {
 				  if (mail($email2, $subjectTo, $corpsEmailTo, $headers)) {
 						$envoie_mail_bdd = $conn_portfolio->prepare("INSERT INTO historique_email (nom, email, message) VALUES (:nom, :email, :message)");
