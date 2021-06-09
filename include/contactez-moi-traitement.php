c7d090d420cea7b869874219c3fdfaa39d26a104
arcausin <82374375+arcausin@users.noreply.github.com>
2021-06-09 10:39:30 +0200

    ahah c'est marrant

--- include/contactez-moi-traitement.php
+++ include/contactez-moi-traitement.php
@@ -48,10 +48,10 @@ if (isset($_POST['submit'])) { // si le bouton Envoyer est appuyé
 				$corpsEmailFrom = "
 				<html>
 					<head>
-						<title>". $subjectFrom ."</title>
+						<title>". $subjectTo ."</title>
 					</head>
 					<body>
-						". $messageFrom ."
+						". $messageTo ."
 					</body>
 				</html>
 				";
