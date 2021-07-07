39f6be96a5c02919abe7af8fc818f70ff18e180d
arcausin <82374375+arcausin@users.noreply.github.com>
2021-07-07 09:51:18 +0200

    mise a jour utilisation de la fonction ob_get et remplacement de else if par elseif

--- include/contactez-moi-traitement.php
+++ include/contactez-moi-traitement.php
@@ -1,5 +1,6 @@
 <?php
 include ("../db_portfolio.php");
+
 function validationDonnees($donnees) {
 	$donnees = trim($donnees); // Supprime les espaces (ou d'autres caractères) en début et fin de chaîne
 	$donnees = stripslashes($donnees); // Supprime les antislashs d'une chaîne
@@ -7,87 +8,116 @@ function validationDonnees($donnees) {
 	return $donnees;
 }
 
-if (isset($_POST['submit'])) { // si le bouton Envoyer est appuyé
-	if (empty($_POST['name'])) { // si le champ Nom n'est pas rempli
+if (isset($_POST['submit'])) {
+	if (empty($_POST['name'])) {
 		header('Location: ../?nom#contactez-moi');
-	} else {
-		if (empty($_POST['email'])) { // si le champ Email n'est pas rempli
-			header('Location: ../?email#contactez-moi');
-		} else {
-			if (empty($_POST['message'])) { // si le champ Message n'est pas rempli
-				header('Location: ../?message#contactez-moi');
-			} else {
-				// tout les champs sont correctement rempli
-				// on récupère et sécurise les informations envoyé par le formulaire de contact
-				$name = validationDonnees($_POST['name']);
-				$email = validationDonnees($_POST['email']);
-				$message = validationDonnees($_POST['message']);
-				// on prépare le mail
-				$headers = "MIME-Version: 1.0" . "\r\n";
-				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		exit();
+	} // si le champ Nom n'est pas rempli
+	elseif (empty($_POST['email'])) {
+		header('Location: ../?email#contactez-moi');
+		exit();
+	} // si le champ Email n'est pas rempli
+	elseif (empty($_POST['message'])) {
+		header('Location: ../?message#contactez-moi');
+		exit();
+	} // si le champ Message n'est pas rempli
+	else {
+		// tout les champs sont correctement rempli
+		// on récupère et sécurise les informations envoyé par le formulaire de contact
+		$name = validationDonnees($_POST['name']);
+		$email = validationDonnees($_POST['email']);
+		$message = validationDonnees($_POST['message']);
+		// on prépare le mail
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-				$subjectFrom = "Nouveau message de " . $name;
+		$subjectFrom = "Nouveau message de " . $name;
 
-				$subjectTo = "Confirmation de reception - Alexis D'Ambrosio";
+		$subjectTo = "Confirmation de reception - Alexis D'Ambrosio";
+/*
+		$messageFrom = "<p>Bonjour, <br/>
+		Vous avez recu un nouveau message de la part de : " . $name . "<br/>
+		E-mail : " . $email . "<br/>
+		<code>Message : " . $message . "</code><br/>
+		Cordialement <br/>
+		Emma <br/><br/>
+		<i style=\"display: none;\">Si vous n'êtes pas à l'origine de cette activité, veuillez <a href=\"http://alexis-dambrosio.fr/#contactez-moi\">me contacter</a>.</i></p>";
+*/
+		ob_start();
+		?>
+		<p>Bonjour,<br/>
+		Vous avez reçu un nouveau message de la part de : <?php echo $name ?><br/>
+		E-mail : <?php echo $email ?><br/>
+		<code>Message : <?php echo $message ?></code><br/>
+		Cordialement<br/>
+		Emma</p>
+		<?php
+		$messageFrom = ob_get_clean();
 
-				$messageFrom = "<p>Bonjour, <br/>
-				Vous avez recu un nouveau message de la part de : " . $name . "<br/>
-				E-mail : " . $email . "<br/>
-				<code>Message : " . $message . "</code><br/>
-				Cordialement <br/>
-				Emma <br/><br/>
-				<i style=\"display: none;\">Si vous n'êtes pas à l'origine de cette activité, veuillez <a href=\"http://alexis-dambrosio.fr/#contactez-moi\">me contacter</a>.</i></p>";
+		ob_start();
+		?>
+		<p>Bonjour,<br/>
+		Nous vous confirmons avoir reçu votre message de la part de : <?php echo $name ?><br/>
+		E-mail : <?php echo $email ?><br/>
+		<code>Message : <?php echo $message ?></code><br/>
+		Cordialement<br/>
+		Alexis D'Ambrosio<br/><br/>
+		<i>Si vous n'êtes pas à l'origine de cette activité, veuillez <a href="http://alexis-dambrosio.fr/#contactez-moi">me contacter</a>.</i></p>
+		<?php
+		$messageTo = ob_get_clean();
 
-				$messageTo = "<p>Bonjour, <br/>
-				Nous vous confirmons avoir reçu votre message de la part de : " . $name . "<br/>
-				E-mail : " . $email . "<br/>
-				<code>Message : " . $message . "</code><br/>
-				Cordialement <br/>
-				Alexis D'Ambrosio <br/><br/>
-				<i>Si vous n'êtes pas à l'origine de cette activité, veuillez <a href=\"http://alexis-dambrosio.fr/#contactez-moi\">me contacter</a>.</i></p>";
+		ob_start();
+		?>
+		<html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+				<title><?php echo $subjectFrom ?></title>
+			</head>
+			<body>
+				<?php echo $messageFrom ?>
+			</body>
+		</html>
+		<?php
+		$corpsEmailFrom = ob_get_clean();
 
-				$corpsEmailFrom = "
-				<html>
-					<head>
-						<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
-						<title>". $subjectFrom ."</title>
-					</head>
-					<body>
-						". $messageFrom ."
-					</body>
-				</html>
-				";
+		ob_start();
+		?>
+		<html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+				<title><?php echo $subjectTo ?></title>
+			</head>
+			<body>
+				<?php echo $messageTo ?>
+			</body>
+		</html>
+		<?php
+		$corpsEmailTo = ob_get_clean();
 
-				$corpsEmailTo = "
-				<html>
-					<head>
-						<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
-						<title>". $subjectTo ."</title>
-					</head>
-					<body>
-						". $messageTo ."
-					</body>
-				</html>
-				";
-				// envoie du mail
-				if (mail("arcausin@gmail.com, adao.dambrosio@gmail.com", $subjectFrom, $corpsEmailFrom, $headers)) {
-				  if (mail($email, $subjectTo, $corpsEmailTo, $headers)) {
-						$envoie_mail_bdd = $conn_portfolio->prepare("INSERT INTO historique_email (nom, email, message, date_ajout) VALUES (:nom, :email, :message, NOW())");
-						$envoie_mail_bdd->execute(array(
-							'nom' => $name,
-							'email' => $email,
-							'message' => $message
-						));
-				    header('Location: ../?envoyer#contactez-moi');
-				  } else {
-				  	header('Location: ../?renvoyer#contactez-moi');
-				  }
-				} else {
-					header('Location: ../?renvoyer#contactez-moi');
-				}
-			}
-		}
-	}
-} else {
+		// envoie du mail
+		if (mail("arcausin@gmail.com, adao.dambrosio@gmail.com", $subjectFrom, $corpsEmailFrom, $headers)) {
+		  if (mail($email, $subjectTo, $corpsEmailTo, $headers)) {
+				$envoie_mail_bdd = $conn_portfolio->prepare("INSERT INTO historique_email (nom, email, message, date_ajout) VALUES (:nom, :email, :message, NOW())");
+				$envoie_mail_bdd->execute(array(
+					'nom' => $name,
+					'email' => $email,
+					'message' => $message
+				));
+		    header('Location: ../?envoyer#contactez-moi');
+				exit();
+		  } // si les deux mails sont envoyé
+			else {
+		  	header('Location: ../?renvoyer#contactez-moi');
+				exit();
+		  } // si le second mail n'est pas envoyé
+		} // si le premier mail est envoyé
+		else {
+			header('Location: ../?renvoyer#contactez-moi');
+			exit();
+		} // si le premier mail n'est pas envoyé
+	} // si toutes les données sont envoyé
+} // si le bouton Envoyer est appuyé
+else {
 	header('Location: ../?renvoyer#contactez-moi');
-}
+	exit();
+} // si le bouton envoyé n'est pas appuyé
